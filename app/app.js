2f2f9cec54f6cf40428bf82dc8af6399b644f7db
Łukasz Broś <lukasz.bros.sp@gmail.com>
2016-01-03 14:49:59 +0100

    Most game festures

--- app/app.js
+++ app/app.js
@@ -1,3 +1,1 @@
 var myApp = angular.module("myApp",[]);
-
-var zobaczmy = 'biedronka';
--- app/controllers/mainctrl.js
+++ app/controllers/mainctrl.js
@@ -4,15 +4,15 @@ myApp.controller('mainController', ['$scope', function($scope) {
 	var wordsLottery = function(){
 		var rand = wordsArray[Math.floor(Math.random() * wordsArray.length)];
 		return randomizedValue = rand.toUpperCase();	
-	}();
-
+	}();	
 
 	$scope.randomValue = randomizedValue;
 
-	$scope.buttonArray = [];
+	$scope.buttonsArray = [];
 	$scope.wordsArray = ['piesa', 'kota', 'mysliwya'];
+	$scope.missedArray = [];
 
-	$scope.logArray = $scope.buttonArray;
+
 
 	var primaryArray = [];
 	var secondaryArray = [];
@@ -25,23 +25,62 @@ myApp.controller('mainController', ['$scope', function($scope) {
 	}();
 
 	$scope.primaryArray = primaryArray;
+	$scope.lettersMatched = [];
 
 	$scope.keyPress = function(keyCode){
    		var code = keyCode;
    		var res = String.fromCharCode(keyCode);
-   		code > 64 && code < 91 ? ($scope.buttonClicked = res, $scope.buttonArray.push($scope.buttonClicked + ", ")) : console.log(res + " to nie litera");
+   		var count = 0;
 		for (var g = 0, h = randomizedValue.length; g < h; g++) {
 			if (res === randomizedValue.charAt(g)) {
 				primaryArray[g] = res;
-			} else{console.log('not' + g)}
+				console.log('The letter is on the positin ' + g);
+				if ($scope.buttonsArray.indexOf(res + ", ") >= 0) {
+					console.log('aa')
+				} else {
+					$scope.lettersMatched.push(res);
+				}
+				
+			} else {
+				console.log('not on position ' + g)
+				count++;
+			}
 		};
-	}
+
+		var repetition = function (){
+			var countRepeat = $scope.buttonsArray.indexOf(res + ", ");
+			if (count == randomizedValue.length && (code > 64 && code < 91)) {
+			
+			console.log('Nie ma takiej litery');
+			if (countRepeat >= 0) {
+				console.log('It is still a wrong letter');
+			} else {
+				$scope.missedArray.push(res + ", ");
+				countRepeat++;
+			}
+			
+		} else {
+			
+			if (countRepeat >= 0) {
+				console.log('The existing letter has already been clicked')
+			} else {
+				console.log("Letter " + res + " finds in the word for the first time");
+			}
+			
+		};
+		}();
+		
+
+		code > 64 && code < 91 ? ($scope.buttonClicked = res, $scope.buttonsArray.push($scope.buttonClicked + ", ")) : console.log(res + " to nie litera");
+
+		var ukonczono = function(){
+		if ($scope.lettersMatched.length == $scope.primaryArray.length) {
+			console.log('You have won the game');
+		};
+	}();
 
 
+	};
 
 	
-
-	var fillWithLetters = function(){
-		
-	};
 }]);
