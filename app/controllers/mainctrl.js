5ea9503d5653767ab157af1e23b461582bdde333
Łukasz Broś <lukasz.bros.sp@gmail.com>
2016-01-05 15:22:19 +0100

    upadate

--- app/controllers/mainctrl.js
+++ app/controllers/mainctrl.js
@@ -4,8 +4,14 @@ myApp.controller('mainController', ['$scope', function($scope) {
 	var wordsLottery = function(){
 		var rand = wordsArray[Math.floor(Math.random() * wordsArray.length)];
 		return randomizedValue = rand.toUpperCase();	
-	}();	
+	}();
 
+	var wordsLottery2 = function(){
+		var rand = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+		return randomizedValue = rand.toUpperCase();	
+	};	
+
+	$scope.wordsLotteryMechanizm = wordsLottery2;
 	$scope.randomValue = randomizedValue;
 
 	$scope.buttonsArray = [];
@@ -18,7 +24,9 @@ myApp.controller('mainController', ['$scope', function($scope) {
 	$scope.hangEvent = {
 		array: [],
 		imgAdd: function(){
-			this.array.push({url: 'assets/img/order/0' + this.array.length + '.png', imgClass: 'imgBox__hangImages--img0' + this.array.length});
+			if ($scope.mistakesNumber < 12) {
+				this.array.push({url: 'assets/img/order/0' + this.array.length + '.png', imgClass: 'imgBox__hangImages--img0' + this.array.length});
+			}	
 		}
 		};
 
@@ -38,7 +46,10 @@ myApp.controller('mainController', ['$scope', function($scope) {
 	$scope.primaryArray = primaryArray;
 	$scope.lettersMatched = [];
 
+	$scope.winsCounter = 0;
+
 	$scope.keyPress = function(keyCode){
+		console.log(keyCode);
    		var code = keyCode;
    		var res = String.fromCharCode(keyCode);
    		var count = 0;
@@ -78,10 +89,23 @@ myApp.controller('mainController', ['$scope', function($scope) {
 		
 		code > 64 && code < 91 ? ($scope.buttonClicked = res, $scope.buttonsArray.push($scope.buttonClicked + ", ")) : console.log(res + " to nie litera");
 
+		
+
 		var ukonczono = function(){
 			if ($scope.lettersMatched.length == $scope.primaryArray.length && $scope.mistakesNumber < 11) {
-				setTimeout(function(){ alert("You have won the game"); }, 100);
-				setTimeout(function(){location.reload(); }, 1000);
+				setTimeout(function(){ alert("CONGRATULATIONS! You have won the game!"); }, 200);
+
+				$scope.winsCounter++;
+				$scope.buttonsArray = [];
+				$scope.missedArray = [];
+				$scope.hangEvent.array = [];
+				$scope.lettersMatched = [];
+				$scope.wordsLotteryMechanizm();
+				$scope.emptyCells = [];
+				$scope.emptyCells2();
+				$scope.mistakesNumber = 0;
+				$scope.makeEmpty();
+
 
 			} else if ($scope.mistakesNumber >= 11) {
 				for (var i = 0, x=randomizedValue.length; i < x; i++) {
@@ -89,10 +113,16 @@ myApp.controller('mainController', ['$scope', function($scope) {
 
 					primaryArray[i] =  randomizedValue.charAt(i);
 				};
-				$scope.gameOverClasses = 'gameOver';
+				$scope.gameOverClasses.windowApear = 'gameOver';
+				$scope.gameOverClasses.text02 = 'gameOver__text gameOver__text--02'
 				
 			}
 		}();
+
+		if (keyCode == 13 && $scope.mistakesNumber >= 11) {
+			console.log('Kliknąłeś enter');
+			$scope.pageReload();
+		};
 	};
 
 	$scope.emptyCells = [];
@@ -108,7 +138,20 @@ myApp.controller('mainController', ['$scope', function($scope) {
 				$scope.emptyCells.push('_');
 			}
 		}();
+
+	var emptyCells2 = function(){
+			var countCells = (11 - randomizedValue.length);
+			for(var i = 0, x = countCells; i<x; i++){
+				console.log('puste są: ' + i);
+				$scope.emptyCells.push('_');
+			}
+		};
+
+	$scope.emptyCells2 = emptyCells2;
 	
-	$scope.gameOverClasses = 'gameOver gameOver--hidden';
+	$scope.gameOverClasses = {
+		windowApear: 'gameOver gameOver--hidden',
+		text02: 'gameOver__text gameOver__text--02 is-gameOver__text--02--hidden'
+	};
 
 }]);
